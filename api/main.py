7104573d93b4bafad17c6fcc4655151dda047cb5
S1GHE <golikovefim65@gmail.com>
2025-01-20 10:47:05 +0300

    feat: docker-compose

--- api/main.py
+++ api/main.py
@@ -1,10 +1,21 @@
-from flask import Flask, jsonify, request
+import os
+
+from flask import Flask, jsonify
+
+API_PORT = int(os.getenv('API_PORT', '8000'))
 
 app = Flask(__name__)
 
-@app.route("/api/data", methods=['GET'])
-def data ():
-    return  jsonify({"msg": "init api-app"}), 200
 
-if __name__ == "__main__":
-    app.run(port=8000)
+@app.route('/', methods=['GET'])
+def index():
+    return jsonify({"index": "index"})
+
+
+@app.route('/api/data', methods=['GET'])
+def data():
+    return jsonify({'msg': 'init api-app'}), 200
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=API_PORT)
