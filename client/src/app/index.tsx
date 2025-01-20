ca77338effa59df4d36b93786cc998900f335001
S1GHE <golikovefim65@gmail.com>
2025-01-20 07:27:56 +0300

    feat: инциализация react-app

---
+++ client/src/app/index.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@/components/ui/button";
+
+import "../index.css";
+
+export const App = () => {
+  return (
+    <div>
+      <Button>init project</Button>
+    </div>
+  );
+};
