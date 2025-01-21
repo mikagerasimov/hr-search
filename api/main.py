e1fbc541f690cffd27e6c39a07055d646afe2b66
Mikhail <160683388+mikagerasimov@users.noreply.github.com>
2025-01-21 22:11:49 +0300

    Add files via upload

--- api/main.py
+++ api/main.py
@@ -1,6 +1,8 @@
 from config import Config
 from flask import Flask, jsonify
 from flask_cors import CORS
+from pipeline import working
+from os import path
 
 cfg = Config()
 app = Flask(__name__)
@@ -18,3 +20,9 @@ def data():
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=cfg.api_port)
+
+
+#Запуск пайплайна, не работает, не знаю почему
+    excel = path.join()
+    results = working(excel)
+    print(results)
